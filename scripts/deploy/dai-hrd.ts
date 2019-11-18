06dfaabbc7296fe0faf71ba3828ae36849e2160a
Micah Zoltu <micah@zoltu.net>
2019-11-18 14:22:11 +0800

    Adds code for mainnet deployment.

--- scripts/deploy/dai-hrd.ts
+++ scripts/deploy/dai-hrd.ts
@@ -2,7 +2,7 @@ import { deployContract } from './helpers'
 import { JsonRpc } from '@zoltu/ethereum-types'
 import { DaiHrd, Dependencies } from '../../generated/dai-hrd'
 
-export async function deployDaiHrd(rpc: JsonRpc, dependencies: Dependencies) {
-	const address = await deployContract(rpc, 'dai-hrd', 'DaiHrd.sol', 'DaiHrd', 0n)
+export async function deployDaiHrd(rpc: JsonRpc, dependencies: Dependencies, ensReverseRegistrarAddress: bigint) {
+	const address = await deployContract(rpc, 'dai-hrd', 'DaiHrd.sol', 'DaiHrd', ensReverseRegistrarAddress)
 	return new DaiHrd(dependencies, address)
 }
